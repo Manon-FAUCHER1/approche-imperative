eaebcc75ae4183c1a98dfeb8fc6fd2cf334bf65e
manon <manon.faucher.pro@gmail.com>
2021-01-25 14:43:53 +0100

    finish AffichagePartiel

--- src/fr/algorithmie/IversionContenu.java
+++ src/fr/algorithmie/IversionContenu.java
@@ -16,15 +16,16 @@ public class IversionContenu {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;	
 	
+		/* Créer un tableau arrayCopy et copier tous les éléments de array dans arrayCopy mais
+		dans l’ordre inverse.*/
 		int[] arrayCopy = IntStream.rangeClosed(1, array.length).map(i -> array[array.length-i]).toArray();
 
+		// Afficher l’ensemble des éléments des 2 tableaux
 		System.out.println(Arrays.toString(array) + "\n" + Arrays.toString(arrayCopy));
 	
-		
-	}
+		}
 
 }
