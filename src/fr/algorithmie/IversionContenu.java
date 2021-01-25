bf7fd471a84efbd2884d3ad5b602506dc1ee360f
manon <manon.faucher.pro@gmail.com>
2021-01-25 13:43:56 +0100

    finish IversionContenu

--- src/fr/algorithmie/IversionContenu.java
+++ src/fr/algorithmie/IversionContenu.java
@@ -3,6 +3,9 @@
  */
 package fr.algorithmie;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * @author manon
  *
@@ -15,7 +18,12 @@ public class IversionContenu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;	
+	
+		int[] arrayCopy = IntStream.rangeClosed(1, array.length).map(i -> array[array.length-i]).toArray();
+
+		System.out.println(Arrays.toString(array) + "\n" + Arrays.toString(arrayCopy));
+	
 		
 	}
 
