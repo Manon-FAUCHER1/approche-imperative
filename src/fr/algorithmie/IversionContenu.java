b8b4ac32b4e59a8aa8aaa7aadc7dc51a4e9d6161
manon <manon.faucher.pro@gmail.com>
2021-01-25 11:54:40 +0100

    Create package fr.algorithmie create class AffichageInverse create class IversionContenu

---
+++ src/fr/algorithmie/IversionContenu.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package fr.algorithmie;
+
+/**
+ * @author manon
+ *
+ */
+public class IversionContenu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+		
+	}
+
+}
--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -15,26 +15,29 @@ public class DeclarationApp {
 	public static void main(String[] args) {
 
 		// DECLAREZ LES VARIABLES
-		byte age = 21;
-		short b = 12300;
-		int codePostale = 44300;
-		long d= 36415893L;
-		float e = 2.9F;
-		double f = -123.513;
-		char lastName = 'F';
-		boolean h = true;
 		String fistName = "Manon";
+		char lastName = 'F';
+		byte age = 21;
+		float taille = 1.65F;
+		int codePostale = 44300;
+		
+		short b = 053;
+		long d= 36415893L;
+		double f = -123.513;
+		boolean h = true;
 		
 		//AFFICHAGE DE LA VALEUR D’UNE VARIABLE
-		System.out.println(age);
-		System.out.println(b);
-		System.out.println(codePostale);
-		System.out.println(d);
-		System.out.println(e);
-		System.out.println(f);
-		System.out.println(lastName);
-		System.out.println(h);
 		System.out.println(fistName);
+		System.out.println(lastName);
+		System.out.println(taille);
+		System.out.println(age);
+		System.out.println(codePostale);
+		
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(h);
+		
 		
 		// Retour à la ligne dans une chaine de caractère
 		String randomString = "Voici le résultat d’un calcul : 1+5=6";
